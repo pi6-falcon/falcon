38e27c7d77ac98270b85c7d93a7511785c9db40c
eopit <pedro.barricelli@mercadolivre.com>
2021-09-14 21:54:25 -0300

    [FALCON-30] Adding first implementation about clean architecture

---
+++ src/main/kotlin/com/falcon/falcon/core/usecase/product/CreateProduct.kt
@@ -0,0 +1,25 @@
+package com.falcon.falcon.core.usecase.product
+
+import com.falcon.falcon.core.entity.Product
+import com.falcon.falcon.dataprovider.persistence.ProductDataProvider
+import mu.KotlinLogging
+import org.springframework.stereotype.Service
+
+interface CreateProduct {
+    fun createProduct()
+}
+
+@Service
+class CreateProductImpl(private val productDataProvider: ProductDataProvider) : CreateProduct {
+
+    private val log = KotlinLogging.logger {}
+
+    override fun createProduct() {
+        log.info { "creating product..." }
+        val product = Product("this-product-is-from-core-package")
+        productDataProvider.saveProduct() // repository abstraction
+
+        // business rules
+    }
+
+}
