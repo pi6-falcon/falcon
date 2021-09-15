136950a99d8b114424a165fc1bc4d54edc50d7ef
rafaelnarbutis <rafanarbutis@outlook.com>
2021-09-14 22:51:47 -0300

    changing  url enconde strategy

--- src/main/kotlin/com/falcon/falcon/utils/UrlUtils.kt
+++ src/main/kotlin/com/falcon/falcon/utils/UrlUtils.kt
@@ -1,18 +1,11 @@
 package com.falcon.falcon.utils
 
 import com.google.common.io.BaseEncoding
-import org.apache.tomcat.util.codec.binary.BaseNCodec
 import java.util.*
 
 object UrlUtils {
 
-    private const val DELIMITER = '-';
-
-    private const val ZERO = 0;
-
-    private const val SIX = 6;
-
     fun generate() = BaseEncoding.base64Url().encode(
-            UUID.randomUUID().toString().split(DELIMITER)[ZERO].encodeToByteArray()).substring(ZERO, SIX)
+        UUID.randomUUID().toString().toByteArray()).substring(0, 6)
 
 }
