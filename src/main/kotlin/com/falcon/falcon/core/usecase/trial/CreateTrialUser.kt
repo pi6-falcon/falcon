86ee68917b6c342f23eda4fbe23feed8532618d9
eopit <pedro.barricelli@mercadolivre.com>
2021-10-23 15:31:02 -0300

    Fixing tests

--- src/main/kotlin/com/falcon/falcon/core/usecase/trial/CreateTrialUser.kt
+++ src/main/kotlin/com/falcon/falcon/core/usecase/trial/CreateTrialUser.kt
@@ -3,13 +3,13 @@ package com.falcon.falcon.core.usecase.trial
 import com.falcon.falcon.core.entity.User
 import com.falcon.falcon.core.enumeration.UserType
 import com.falcon.falcon.core.usecase.user.CreateUserUseCase
-import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.security.SecureRandom
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.UUID
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.streams.asSequence
 
 interface CreateTrialUser {
 
@@ -22,13 +22,22 @@ class CreateTrialUserImpl(
     @Value("\${api.trial.default-expiration-in-hour}")
     private var trialDurationInHour: Long,
 ) : CreateTrialUser {
+
+    private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
     override fun execute(): User =
         createUser.execute(
             User(
                 username = UUID.randomUUID().toString(),
-                password = RandomStringUtils.random(24, 0, 0, true, true, null, SecureRandom()),
+                password = generateSecureRandomPassword(24),
                 type = UserType.TRIAL,
                 expirationDate = Instant.now().plus(trialDurationInHour, ChronoUnit.HOURS)
             )
         )
+
+    private fun generateSecureRandomPassword(length: Long) = ThreadLocalRandom.current()
+        .ints(length, 0, charPool.size)
+        .asSequence()
+        .map(charPool::get)
+        .joinToString("")
 }
